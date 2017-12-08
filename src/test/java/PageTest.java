f53040873f88a5bc04a7667593ff1986e42853cd
ilya <ilya.chekasin93@yandex.ru>
2017-12-08 21:17:49 +0500

    first commit

--- src/test/java/PageTest.java
+++ src/test/java/PageTest.java
@@ -1,75 +1,87 @@
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
-import java.io.File;
-import java.io.IOException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
 
 
 public class PageTest {
 
     WebDriver driver;
-    By topics = By.id("input-0");
-    By worldwide = By.xpath("//hierarchy-picker[contains(@track-name,\"geoPicker\")]");
-    By regionInput = By.id("input-9");
-    By goinput = By.xpath("//li[contains(@md-virtual-repeat,\"item in $mdAutocompleteCtrl.matches\")][@role=\"button\"]");
-    By timeFilter = By.xpath("//md-select-value[contains(@class,\"_md-select-value\")]");
-    By for90Days = By.xpath("//div[contains(text(),\"За 90 дней\")]/parent::*");
-    By subfilterPopularityBySubregion = By.xpath("//md-select[contains(@aria-label,\"Субрегион\")]");
-    By city = By.xpath("//md-option[@value='city']");
-    By error = By.xpath("//p[contains(@class,\"widget-error-title\")]");
-
-    By up = By.xpath("//input[@id=\"input-25\"]");
-    By DynamicsOfPopularity = By.xpath("//div[contains(@class,\"fe-line-chart-header-title\")]");
-    By PopularityByRegion = By.xpath("//div[contains(text(),\"Популярность по субрегионам\")]");
-    By MoreOnTheTopicAndInTheTrend = By.xpath("//div[contains(@class,\"fe-atoms-generic-title\")][contains(text(),\"Ещё по теме\")]");
-
+    By payments = By.xpath("//li[contains(@data-menu-item,\"2\")]");
+    By communalPayments = By.xpath("//a[contains(@title,\"Коммунальные платежи\")]");
+    By regions = By.xpath("//span[contains(@class,\"ui-link payment-page__title_inner\")]");
+    By moscow = By.xpath("//span[text()=\"г. Москва\"]");
+    By serviceProviders = By.xpath("//ul[contains(@class,\"ui-menu ui-menu_icons UIPayments__categoryProviders_3Fsrs\")]/li");
+    By internalProviders = By.xpath("span/a");
+    By PayHousingServicesInMoscow = By.xpath("//span[text()=\"Оплатить ЖКУ в Москве\"]/parent::*");
+    By input = By.xpath("//input[contains(@class,\"Input__field_2XFoD Input__field_align_left_1phV_\")]");
+    By dropDownList = By.xpath("//div[contains(@class,\"Grid__root_2zhCT Grid__root_display_block_2b7Gu\")]/div");
+    By internalDropDownList = By.xpath("div/div/div");
+    By stPetersburg = By.xpath("//span[text()=\"г. Санкт-Петербург\"]");
+    By wanted = By.xpath("//div[text()=\"ЖКУ-Москва\"]");
+    By payerCode = By.xpath("//input[@name=\"provider-payerCode\"]");
+    By payerCodeError = By.xpath("//div[@data-qa-file=\"UIInput\"]/parent::*/div[@data-qa-file=\"UIFormRowError\"]");
+    By providerPeriod = By.xpath("//input[@name=\"provider-period\"]");
+    By providerPeriodError = By.xpath("//div[@data-qa-file=\"UIInputDate\"]/parent::*/div[@data-qa-file=\"UIFormRowError\"]");
+    By amountOfPayment = By.xpath("//div[@data-qa-file=\"FormFieldSet\"]//input");
+    By amountOfPaymentError = By.xpath("//div[@data-qa-file=\"FormFieldSet\"]//div[@data-qa-file=\"UIFormRowError\"]");
 
     public PageTest (WebDriver driver){
         this.driver = driver;
     }
 
-    //Поисковой запрос
-    public void SetInTopics(String topic){
-        WebElement elementtopic = driver.findElement(topics);
-        elementtopic.sendKeys(topic);
-        elementtopic.sendKeys(Keys.ENTER);
+    public String getErrorMasage (By Input, By error, String inputVal) throws InterruptedException {
+        expectation(Input);
+        WebElement elementInput = driver.findElement(Input);
+        elementInput.sendKeys(inputVal);
+        elementInput.sendKeys(Keys.ENTER);
+
+        expectation(error);
+        WebElement elementError = driver.findElement(error);
+        return elementError.getText();
     }
 
-    //Фильтр по региону
-    public void FilterByRegion(String region) throws InterruptedException {
-        WebElement woldwide = driver.findElement(worldwide);
-        woldwide.click();
-
-        WebElement inputreg = driver.findElement(regionInput);
-        inputreg.sendKeys(region);
-
-        WebElement russia = driver.findElement(goinput);
-        russia.click();
+    public void changeRegion(By region, By cityToChange, String city) throws InterruptedException {
+        expectation(region);
+        WebElement elementRegions = driver.findElement(region);
+        String regionText = elementRegions.getText();
+        if(regionText.equals(city)) {}
+        else{
+            elementRegions.click();
+            expectation(cityToChange);
+            WebElement moskowElement = driver.findElement(cityToChange);
+            moskowElement.click();
+        }
     }
 
-    //Фильтр по времени
-    public void TimeFilter(By time) throws InterruptedException {
-        WebElement timefilt = driver.findElement(timeFilter);
-        timefilt.click();
-
-        WebElement ElementTime = driver.findElement(time);
-        ElementTime.click();
+    public void ClickOnElement(By element) throws InterruptedException {
+        expectation(element);
+        WebElement paymentsElement = driver.findElement(element);
+        paymentsElement.click();
     }
 
-    //Фильтр по городу в окне “Популярность по субрегионам”
-    public void FilterByCityOrRegion (By cityOrRegion) throws InterruptedException {
-        WebElement subFilter = driver.findElement(subfilterPopularityBySubregion);
-        subFilter.click();
-
-        WebElement elementCity = driver.findElement(cityOrRegion);
-        elementCity.click();
+    public String getServiceProviderName (WebElement serviceProvider){
+        String serviceProviderName = serviceProvider.getAttribute("title");
+        return serviceProviderName;
     }
 
-    public String ErrorCheck (By errorElement) {
-        WebElement ElementError = driver.findElement(errorElement);
-        String errorText = ElementError.getText();
-        return errorText;
+    public WebElement getFirstElement (By listElements, By internalLocator) {
+        List<WebElement> ElementsList = driver.findElements(listElements);
+        WebElement firstOnList = ElementsList.get(0);
+        WebElement first = firstOnList.findElement(internalLocator);
+        return first;
     }
 
+     public boolean verificationOfSoughtSupplier (){
+        WebElement providerListed;
+        for (WebElement serviceProvidersElement:(driver.findElements(serviceProviders))) {
+            providerListed = serviceProvidersElement.findElement(By.tagName("span")).findElement(By.tagName("a"));
+            if (providerListed.getAttribute("title").equals("ЖКУ-Москва")) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public void scrollWithOffset(By scrollBy, int x, int y) {
 
@@ -78,12 +90,11 @@ public class PageTest {
                 + (ByScroll.getLocation().y - y) + ");";
 
         ((JavascriptExecutor)driver).executeScript(code, ByScroll, x, y);
-
     }
 
-    public void skrin (String name) throws IOException {
-        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        String pathname = "C:\\test\\" + name + ".png";
-        FileUtils.copyFile(scrFile, new File(pathname));
+    public void expectation(By element) throws InterruptedException {
+        WebDriverWait dynamicElement = new WebDriverWait(driver, 10);
+        dynamicElement.until(ExpectedConditions.presenceOfElementLocated(element));
+        Thread.sleep(3000);
     }
 }
